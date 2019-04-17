f60032e7606a2c7fb7347017f0628150a60b6b57
lucas <lucasaraujoss95@gmail.com>
2019-04-17 20:59:11 -0300

    Estuturação de pastas

--- index.php
+++ index.php
@@ -1,92 +1,31 @@
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<title>Green Alert - Sistema de alerta de data</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Ambietec</title>
 
-<form method="POST" action="cadastroEmp.php">
-  <div class= "form-group">
-    <label for="cnpj">CNPJ</label>
-    <input type="text" class="form-control" id="cnpj" placeholder="Infome o CNPJ da empresa">
-  </div>
+  <link rel="stylesheet" href="./css/css-index.css">  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
   
-  <div class="form-group">
-    <label for="numInscricao">Número de Inscrição</label>
-    <input type="text" class="form-control" id="numInscricao" placeholder="Número de inscrição">
-  </div>
-  
-  <div class="form-group">
-    <label for="nomeEmpresa">Nome da Empresa</label>
-    <input type="text" class="form-control" id="nomeEmpresa" placeholder="Nome da empresa">
-  </div>
-  
-  <div class="form-group">
-    <label for="nomeFantasia">Nome Fantasia</label>
-    <input type="email" class="form-control" id="nomeFantasia" placeholder="Nome fantasia">
-  </div>
-  
-  <div class="form-group">
-    <label for="Endereco">Endereço</label>
-    <input type="text" class="form-control" id="Endereco" placeholder="Endereço">
-  </div>
-  
-  <div class="form-group">
-    <label for="bairro">Bairro</label>
-    <input type="text" class="form-control" id="bairro" placeholder="Bairro">
-  </div>
-  
-  <div class="form-group">
-    <label for="bairro">Cidade</label>
-    <input type="text" class="form-control" id="cidade" placeholder="Cidade">
-  </div>
-  
-  <div class="form-group">
-    <label for="cep">CEP</label>
-    <input type="text" class="form-control" id="cep" placeholder="CEP">
-  </div>
-  
-  <div class="form-group">
-    <label for="uf">UF</label>
-    <input type="text" class="form-control" id="uf" placeholder="UF">
-  </div>
-  
-  <div class="form-group">
-    <label for="telefone">Telefone</label>
-    <input type="text" class="form-control" id="telefone" placeholder="Telefone">
-  </div>
-  
-  
-  <div class="form-group">
-    <label for="telefoneOpc">Telefone Opcional</label>
-    <input type="text" class="form-control" id="telefoneOpc" placeholder="Telefone">
-  </div>
-  
-  <div class="form-group">
-    <label for="descAtv">Principal atividade</label>
-    <input type="text" class="form-control" id="descAtv" placeholder="Descrição da principal atividade">
-  </div>
-  
-  <div class="form-group">
-    <label for="responsavel">Responsável</label>
-    <input type="text" class="form-control" id="responsavel" placeholder="E-mail">
+  <div class="all">
+
+    <div class="topo">
+      
+      
+
+    </div>
+
   </div>
 
-  <div class="form-group">
-    <label for="email">E-mail</label>
-    <input type="text" class="form-control" id="email" placeholder="E-mail">
-  </div>
-  
-  <div class="form-group">
-    <label for="senha">Senha<label>
-    <input type="text" class="form-control" id="senha" placeholder="Senha">
-  </div>
-  
-  <div class="form-group">
-    <label for="confSenha">Confirmar Senha</label>
-    <input type="text"  class="form-control" id="confSenha" placeholder="Confirmar senha">
-	
-  </div>
+</body>
+</html>
 
-  <button type="button" class="btn btn-outline-danger">Cancelar</button>
-  <button type="submit" class="btn btn-success">Cadastrar</button>
-
-</form>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
