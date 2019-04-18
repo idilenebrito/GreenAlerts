6864ee655e6b7a81f9750b33ddc6210ba54bed4f
lucas <lucasaraujoss95@gmail.com>
2019-04-17 23:36:07 -0300

    commit comentado

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 
 
+/* crie um arquivo "exemplo.php" na area geral do explorador de aquivos ali do lado,
+  logo após crie um arquivo "exemplo.css" dentro da pasta css e importe dento do html do "exemplo.php"*/
+
+/* O bloco de comando vem vazio
+  antes de tudo crie a tag "html:5" */
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,16 +14,32 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Ambietec</title>
 
-  <link rel="stylesheet" href="./css/css-index.css">  
+
+  /* fodace eu n sei comentar então fica assim por enquanto
+essa parte é a parte de importação
+a tag "link" importa coisas na parte do hmtl */
+  <link rel="stylesheet" href="./css/css-index.css">   /* no href voce bota o caminho do aquivo que vc quer importar, no meu caso eu estou importanto o css do index */ 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
   
   <div class="all">
+  
+    <div class="navegador">
+    
+    </div>
+
+    <div class="areaLogo">
 
     <div class="topo">
       
       
+
+    </div>
+
+    <div class="area">
+    
+    </div>
 
     </div>
 
@@ -26,6 +48,10 @@
 </body>
 </html>
 
+
+/* fodace eu n sei comentar então fica assim por enquanto
+essa parte é a parte de importação
+a tag "script src" importa coisas na parte do php */
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
