ca1c85e3ca45ec9f78add922f68834811b7dee56
lucas <lucasaraujoss95@gmail.com>
2019-04-18 23:16:13 -0300

    Design de paginas 18-abril

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 
 
-/* crie um arquivo "exemplo.php" na area geral do explorador de aquivos ali do lado,
-  logo após crie um arquivo "exemplo.css" dentro da pasta css e importe dento do html do "exemplo.php"*/
-
-/* O bloco de comando vem vazio
-  antes de tudo crie a tag "html:5" */
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,10 +9,8 @@
   <title>Ambietec</title>
 
 
-  /* fodace eu n sei comentar então fica assim por enquanto
-essa parte é a parte de importação
-a tag "link" importa coisas na parte do hmtl */
-  <link rel="stylesheet" href="./css/css-index.css">   /* no href voce bota o caminho do aquivo que vc quer importar, no meu caso eu estou importanto o css do index */ 
+  
+  <link rel="stylesheet" href="./css/css-index.css">   
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -26,14 +18,46 @@ a tag "link" importa coisas na parte do hmtl */
   <div class="all">
   
     <div class="navegador">
-    
+
+      <div class="navegadorTxt">
+        <div>
+          Navegador
+        </div>
+      </div>
+
+      
+
+      <div class="tracinho">
+       <hr/>
+      </div>
+      
+     <div class="btnTopo">
+     <div class="btn1">
+        <button type="button" class="btn btn-light">Empresas</button>
+      </div>
+
+      <div class="btn2">
+        <button type="button" class="btn btn-light">Documentos</button>
+      </div>
+     </div>
+
     </div>
 
     <div class="areaLogo">
 
     <div class="topo">
       
-      
+      <div class="logo">
+
+      <div class="logoTxt">
+        a
+      </div>
+
+      <div class="logoImg">
+        <img src="https://i.imgur.com/XGpUJgO.png" alt="" width=60px>
+      </div>
+
+      </div>
 
     </div>
 
@@ -49,9 +73,6 @@ a tag "link" importa coisas na parte do hmtl */
 </html>
 
 
-/* fodace eu n sei comentar então fica assim por enquanto
-essa parte é a parte de importação
-a tag "script src" importa coisas na parte do php */
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
